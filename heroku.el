8c44d3db10d5c30345a675ea3e2c81c59c55d5f2
licht1stein <mb@blaster.ai>
2021-06-30 15:34:50 +0300

    Run through checkdoc

--- heroku.el
+++ heroku.el
@@ -1,32 +1,43 @@
+;;; heroku.el --- Emacs client for Heroku
+
+;;; Commentary:
+;;  A simple front-end for Heroku CLI.
+
 (require 'seq)
+
+;;; Code:
 
 (defgroup heroku nil
   "Interact with Heroku CLI from Emacs"
   :prefix "heroku-")
 
-(defvar heroku-app-list nil "List of Heroku apps fetched by heroku.el")
+(defvar heroku-app-list nil "List of Heroku apps fetched by heroku.el.")
 
 (defun heroku--list-service-string-p (s)
-  "Check if a string is a service string"
+  "Check if a string is a service string.
+Argument S Checks if s should be excluded from the app-list."
   (or  (string= "" s) (cl-search "===" s) (cl-search "Process heroku" s)))
 
 (defun heroku--not-list-service-string-p (s)
-  "Check if a sting is NOT a service string"
+  "Check if a sting is NOT a service string.
+Argument S Checks if s is an app name."
   (not (heroku--list-service-string-p s)))
 
 (defun heroku--parse-list (s)
-  "Parse buffer output of heroku-list to produce a list of available apps"
+  "Parse buffer output of `heroku-list' to produce a list of available apps.
+Argument S string to be parsed into a list of app names."
   (let
       ((apps (seq-filter 'heroku--not-list-service-string-p (split-string s "\n"))))
     (mapcar 'car (mapcar 'split-string apps))
     ))
 
 (defun heroku--parse-list-buffer-and-setq (buffer-name)
+  "Parse `BUFFER-NAME` and convert it's contents into a list of app names."
   (setq heroku-app-list
         (heroku--parse-list (with-current-buffer "*Heroku List*" (buffer-substring-no-properties (point-min) (point-max))))))
 
 (defun heroku--update-app-list ()
-  "List all Heroku apps and update heroku-app-list variable"
+  "List all Heroku apps and update `heroku-app-list' variable."
   (message "Getting Heroku apps list...")
   (let ((buffer-name "*Heroku List*"))
     (if (get-buffer buffer-name)
@@ -37,7 +48,7 @@
     (kill-buffer buffer-name)))
 
 (defun heroku-list ()
-  "List all Heroku apps and update heroku-app-list variable"
+  "List all Heroku apps and update `heroku-app-list' variable."
   (interactive)
   (message "Getting Heroku apps list...")
   (let ((buffer-name "*Heroku List*"))
@@ -49,13 +60,15 @@
     (heroku--parse-list-buffer-and-setq buffer-name)))
 
 (defun heroku--read-app-name ()
+  "Prompt user to enter or select an app.
+If `heroku-app-list' is nil, also prompt if user wants to have the app-list updated."
   (unless heroku-app-list
-    (if (y-or-n-p "App list is empty, do you want to update?")
+    (if (y-or-n-p "App list is empty, do you want to update? ")
         (heroku--update-app-list)))
   (completing-read "Enter Heroku app name: " heroku-app-list))
 
 (defun heroku-bash ()
-  "Connect to bash on a Heroku app"
+  "Connect to bash on a Heroku app."
   (interactive)
   (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Bash: %s" app-name)))
     (message (format "Connecting to bash on %s..." app-name))
@@ -64,7 +77,7 @@
     ))
 
 (defun heroku-python ()
-  "Connect to Python on a Heroku app"
+  "Connect to Python on a Heroku app."
   (interactive)
   (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Python: %s" app-name)))
     (message (format "Connecting to Python on %s..." app-name))
@@ -73,7 +86,7 @@
     ))
 
 (defun heroku-logs ()
-  "Connect and stream logs of a Heroku app"
+  "Connect and stream logs of a Heroku app."
   (interactive)
   (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Logs: %s" app-name)))
     (message (format "Gettings Heroku logs for %s..." app-name))
@@ -82,7 +95,7 @@
     (switch-to-buffer-other-window buffer-name)))
 
 (defun heroku-destroy ()
-  "Permanently destroy a Heroku app"
+  "Permanently destroy a Heroku app."
   (interactive)
   (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Destroy: %s" app-name)))
     (if  (and
@@ -94,7 +107,7 @@
       (message "Heroku command canceled"))))
 
 (defun heroku-info ()
-  "Show detailed Heroku app info"
+  "Show detailed Heroku app info."
   (interactive)
   (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Info: %s" app-name)))
     (message (format "Getting info on %s..." app-name))
@@ -103,7 +116,7 @@
     ))
 
 (defun heroku-rename ()
-  "Rename a Heroku app"
+  "Rename a Heroku app."
   (interactive)
   (let* ((app-name (heroku--read-app-name)) (new-name (read-string (format "Enter new name for %s: " app-name))) (buffer-name (format "*Heroku Rename: %s" app-name)))
     (if (gnus-yes-or-no-p (format "Confirm renaming %s -> %s?" app-name new-name))
@@ -114,7 +127,7 @@
     ))
 
 (defun heroku-restart ()
-  "Restart a Heroku app"
+  "Restart a Heroku app."
   (interactive)
   (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Restart Report: %s" app-name)))
     (message (format "Restarting heroku app:  %s..." app-name))
@@ -123,3 +136,7 @@
     ))
 
 (provide 'heroku)
+
+(provide 'heroku)
+
+;;; heroku.el ends here
