e673fa752d7356fd4ce094e8169aa358c2f1d903
licht1stein <mb@blaster.ai>
2021-06-30 15:27:52 +0300

    Will now prompt to update app list if it's empty

--- heroku.el
+++ heroku.el
@@ -25,6 +25,17 @@
   (setq heroku-app-list
         (heroku--parse-list (with-current-buffer "*Heroku List*" (buffer-substring-no-properties (point-min) (point-max))))))
 
+(defun heroku--update-app-list ()
+  "List all Heroku apps and update heroku-app-list variable"
+  (message "Getting Heroku apps list...")
+  (let ((buffer-name "*Heroku List*"))
+    (if (get-buffer buffer-name)
+        (kill-buffer buffer-name))
+    (make-comint-in-buffer "heroku-list" buffer-name "heroku" nil "list")
+    (sleep-for 5)
+    (heroku--parse-list-buffer-and-setq buffer-name)
+    (kill-buffer buffer-name)))
+
 (defun heroku-list ()
   "List all Heroku apps and update heroku-app-list variable"
   (interactive)
@@ -36,6 +47,12 @@
     (switch-to-buffer-other-window buffer-name)
     (sleep-for 5)
     (heroku--parse-list-buffer-and-setq buffer-name)))
+
+(defun heroku--read-app-name ()
+  (unless heroku-app-list
+    (if (y-or-n-p "App list is empty, do you want to update?")
+        (heroku--update-app-list)))
+  (completing-read "Enter Heroku app name: " heroku-app-list))
 
 (defun heroku-bash ()
   "Connect to bash on a Heroku app"
@@ -49,7 +66,7 @@
 (defun heroku-python ()
   "Connect to Python on a Heroku app"
   (interactive)
-  (let* ((app-name (completing-read "Enter Heroku app name: " heroku-app-list)) (buffer-name (format "*Heroku Python: %s" app-name)))
+  (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Python: %s" app-name)))
     (message (format "Connecting to Python on %s..." app-name))
     (make-comint-in-buffer "heroku-python" buffer-name "heroku" nil "run" "python" "-a" app-name)
     (switch-to-buffer-other-window buffer-name)
@@ -58,7 +75,7 @@
 (defun heroku-logs ()
   "Connect and stream logs of a Heroku app"
   (interactive)
-  (let* ((app-name (completing-read "Enter Heroku app name: " heroku-app-list)) (buffer-name (format "*Heroku Logs: %s" app-name)))
+  (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Logs: %s" app-name)))
     (message (format "Gettings Heroku logs for %s..." app-name))
     (make-comint-in-buffer "heroku-logs" buffer-name "heroku" nil "logs" "-t" "-a" app-name)
     (sleep-for 3)
@@ -67,7 +84,7 @@
 (defun heroku-destroy ()
   "Permanently destroy a Heroku app"
   (interactive)
-  (let* ((app-name (completing-read "Enter Heroku app name: " heroku-app-list)) (buffer-name (format "*Heroku Destroy: %s" app-name)))
+  (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Destroy: %s" app-name)))
     (if  (and
           (yes-or-no-p (format "Do you really want destroy %s?" app-name))
           (string-equal app-name (read-string (format  "Type the app name once again to confirm [%s]: " app-name))))
@@ -79,7 +96,7 @@
 (defun heroku-info ()
   "Show detailed Heroku app info"
   (interactive)
-  (let* ((app-name (completing-read "Enter Heroku app name: " heroku-app-list)) (buffer-name (format "*Heroku Info: %s" app-name)))
+  (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Info: %s" app-name)))
     (message (format "Getting info on %s..." app-name))
     (make-comint-in-buffer "heroku-info" buffer-name "heroku" nil "apps:info" "-a" app-name)
     (switch-to-buffer-other-window buffer-name)
@@ -88,7 +105,7 @@
 (defun heroku-rename ()
   "Rename a Heroku app"
   (interactive)
-  (let* ((app-name (completing-read "Enter Heroku app name: " heroku-app-list)) (new-name (read-string (format "Enter new name for %s: " app-name))) (buffer-name (format "*Heroku Rename: %s" app-name)))
+  (let* ((app-name (heroku--read-app-name)) (new-name (read-string (format "Enter new name for %s: " app-name))) (buffer-name (format "*Heroku Rename: %s" app-name)))
     (if (gnus-yes-or-no-p (format "Confirm renaming %s -> %s?" app-name new-name))
         (progn
           (make-comint-in-buffer "heroku-rename" buffer-name "heroku" nil "apps:rename" "-a" app-name new-name)
@@ -99,7 +116,7 @@
 (defun heroku-restart ()
   "Restart a Heroku app"
   (interactive)
-  (let* ((app-name (completing-read "Enter Heroku app name: " heroku-app-list)) (buffer-name (format "*Heroku Restart Report: %s" app-name)))
+  (let* ((app-name (heroku--read-app-name)) (buffer-name (format "*Heroku Restart Report: %s" app-name)))
     (message (format "Restarting heroku app:  %s..." app-name))
     (make-comint-in-buffer "heroku-restart" buffer-name "heroku" nil "restart" "-a" app-name)
     (message (ensure-buffer-string-blocking buffer-name))
